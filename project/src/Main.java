7045ab2643fca1c9c04c038a2648673be3c6278d
d4nkali <133923419+d4nkali@users.noreply.github.com>
2024-05-10 21:08:35 -0300

    Criando estrutura de login e cadastro
    Co-authored-by: JoaoMagalhaes-hub <jgabrielomm@gmail.com>

--- project/src/Main.java
+++ project/src/Main.java
@@ -4,9 +4,50 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
 
+        String c_nome = "", c_senha = "", c_email = "";
+        int c_cpf = 0;
+
+        boolean op_login_success = false;
+        int op_login = 0;
+
         Scanner sc = new Scanner(System.in);
+        Cliente c1 = new Cliente(c_nome, c_cpf, c_email, c_senha);
 
         System.out.println("Bem vindo a Vulture Trades - A Carniça da Blockchain \n");
+        do {
+        
+            System.out.println("Voce deseja Logar [1] ou Cadastra [2]? ");
+            op_login = sc.nextInt();
+            switch (op_login) {
+
+                case 1:
+
+
+                    break;
+
+                case 2:
+
+                    System.out.print("Digite o seu nome: ");
+                    c_nome = sc.next();
+                    System.out.print("Digite o seu CPF: ");
+                    c_cpf = sc.nextInt();
+                    break;
+
+            
+                default:
+
+                    System.out.println("Opção Invalida");
+                    break;
+
+            }
+
+        } while (op_login_success == true);
+        System.out.println("");
+
+        //TODO: Fazer switch case e do while @JANzxz, @Olausz
+
+        sc.close();
 
     }
+
 }
