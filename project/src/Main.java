eeff5785dd66b3c2539bb3aae47e585c6a13fe49
d4nkali <133923419+d4nkali@users.noreply.github.com>
2024-05-11 12:57:46 -0300

    Formatando, adicionando TODO e adicionando email e senha no cadastro

--- project/src/Main.java
+++ project/src/Main.java
@@ -16,24 +16,31 @@ public class Main {
 
         do {
         
-            System.out.println("Voce deseja Logar [1] ou Cadastra [2]? ");
+            System.out.println("Voce deseja Logar [1] ou Cadastrar [2]? ");
             op_login = sc.nextInt();
             switch (op_login) {
 
                 case 1:
+
+                //TODO: Fazer login @JoaoMagalhaes-hub
 
 
                     break;
 
                 case 2:
 
+                //TODO: Terminar cadastro @d4nkali
+
                     System.out.print("Digite o seu nome: ");
                     c_nome = sc.next();
                     System.out.print("Digite o seu CPF: ");
                     c_cpf = sc.nextInt();
+                    System.out.print("Digite um endereço de email: ");
+                    c_email = sc.next();
+                    System.out.print("Digite uma senha: ");
+                    c_senha = sc.next();
                     break;
 
-            
                 default:
 
                     System.out.println("Opção Invalida");
@@ -42,55 +49,67 @@ public class Main {
             }
 
         } while (op_login_success == true);
-        System.out.println("");
 
         //TODO: Fazer switch case e do while @JANzxz, @Olausz
 
-        sc.close();
-
-    System.out.println("Bem-vindo ao Sistema de Operações Bancárias");
-    System.out.println("Escolha uma das seguintes opções:");
-    System.out.println("1 - Extrato");
-    System.out.println("2 - Depósito");
-    System.out.println("3 - Saque");
-    System.out.println("4 - Transferência");
-    System.out.println("5 - Conversor de Moeda");
-    System.out.println("6 - Stake");
-    System.out.println("0 - Sair");
+        System.out.println("Bem-vindo ao Sistema de Operações Bancárias");
+        System.out.println("Escolha uma das seguintes opções:");
+        System.out.println("1 - Extrato");
+        System.out.println("2 - Depósito");
+        System.out.println("3 - Saque");
+        System.out.println("4 - Transferência");
+        System.out.println("5 - Conversor de Moeda");
+        System.out.println("6 - Stake");
+        System.out.println("0 - Sair");
 
         int opcao = sc.nextInt();
 
         switch (opcao) {
+
             case 1:
+
                 System.out.println("Você selecionou: Extrato");
                 break;
 
             case 2:
+
                 System.out.println("Você selecionou: Depósito");
                 break;
 
             case 3:
+
                 System.out.println("Você selecionou: Saque");
                 break;
 
             case 4:
+
                 System.out.println("Você selecionou: Transferência");
                 break;
 
             case 5:
+
                 System.out.println("Você selecionou: Conversor de Moeda");
                 break;
 
             case 6:
+
                 System.out.println("Você selecionou: Stake");
                 break;
 
             case 0:
+
                 System.out.println("Saindo do sistema...");
                 break;
-                
+
             default:
+
                 System.out.println("Opção inválida. Por favor, selecione uma opção válida.");
+                break;
+
         }
+
+    sc.close();
+
     }
+
 }
