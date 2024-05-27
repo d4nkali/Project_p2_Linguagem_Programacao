d46e08633f32645d6cbc05f27541d2d4f725ccf1
d4nkali <133923419+d4nkali@users.noreply.github.com>
2024-05-27 19:37:38 -0300

    Corrigindo Stake
    Co-authored-by: Olausz <142450509+Olausz@users.noreply.github.com>

--- project/src/CarteiraStake.java
+++ project/src/CarteiraStake.java
@@ -2,8 +2,13 @@ import java.util.Scanner;
 
 public class CarteiraStake extends Carteiras implements AcoesStake {
 
-    private int opcao_moeda_depos, opcao_moeda_saque;
-    private float quant_saque = 0, quant_deposito = 0;
+    // Variáveis de opção
+    private int opcao_moeda_depos, opcao_moeda_saque, opcao_moeda_stake;
+    private int quant_meses;
+
+    // Variáveis temporárias 
+    private float quant_saque = 0, quant_deposito = 0, quant_stake = 0;
+    private float rendi_stake;
 
     //* Construtor filho de "CarteiraStake"
     public CarteiraStake() {
@@ -23,7 +28,7 @@ public class CarteiraStake extends Carteiras implements AcoesStake {
     public void depositar(Scanner sc) { //* Criando função para depositar as moedas
 
         System.out.println("Digite o simbolo da moeda/criptomoeda que vai ser depositada.");
-        System.out.println("[1] BTC | [2] ETH | [3] SOL | [4] URC: ");
+        System.out.print("[1] BTC | [2] ETH | [3] SOL | [4] URC: ");
         opcao_moeda_depos = sc.nextInt();
 
         switch (opcao_moeda_depos) {
@@ -46,9 +51,8 @@ public class CarteiraStake extends Carteiras implements AcoesStake {
                 }
 
                 System.out.println("Opção Invalida!");
-                
                 break;
-                
+
             case 2:
 
                 System.out.print("Digite a quantia a ser depositada: ");
@@ -118,7 +122,7 @@ public class CarteiraStake extends Carteiras implements AcoesStake {
     public void sacar(Scanner sc) { //* Criando função para sacar as moedas
 
         System.out.println("Digite o simbolo da moeda/criptomoeda que vai ser sacada.");
-        System.out.println("[1] BTC | [2] ETH | [3] SOL | [4] URC: ");
+        System.out.print("[1] BTC | [2] ETH | [3] SOL | [4] URC: ");
         opcao_moeda_saque= sc.nextInt();
 
         switch (opcao_moeda_saque) {
@@ -141,7 +145,7 @@ public class CarteiraStake extends Carteiras implements AcoesStake {
                 }
 
                 break;
-            
+
             case 2:
 
                 System.out.print("Digite a quantia a ser sacada: ");
@@ -179,7 +183,7 @@ public class CarteiraStake extends Carteiras implements AcoesStake {
                 }
 
                 break;
-                
+
             case 4:
 
                 System.out.print("Digite a quantia a ser sacada: ");
@@ -208,54 +212,55 @@ public class CarteiraStake extends Carteiras implements AcoesStake {
 
     }
 
-    public void stake(Scanner sc) {
+    public void stake(Scanner sc) { //* Criando função para simulação do stake
 
-        System.out.println("Digite a quantidade para investir no stake");
-        float quant_stake = sc.nextFloat();
+        System.out.println("Digite o simbolo da moeda/criptomoeda que vai ser simulada para o stake.");
+        System.out.print("[1] BTC | [2] ETH | [3] SOL | [4] URC: ");
+        opcao_moeda_stake = sc.nextInt();
+        System.out.println();
+    
         System.out.println("Selecione a quantidade de meses para o investimento");
-        System.out.println("[1] 1 mês | [2] 3 mesês | [3] 6 mesês | [4] 1 ano");
-        int quant_meses = sc.nextInt();
+        System.out.print("[1] 1 mês | [2] 3 meses | [3] 6 meses | [4] 1 ano : ");
+        quant_meses = sc.nextInt();
+        System.out.println();
+    
+        System.out.print("Digite a quantidade para simular no stake: ");
+        quant_stake = sc.nextFloat();
+        System.out.println();
 
         switch (quant_meses) {
+
             case 1:
 
-                float investimento1 = quant_stake * 0.01f;
-
-                System.out.println("Parabéns! seu lucro foi de "+ investimento1);
-                
+                rendi_stake = quant_stake * 0.001f;
+                System.out.println("Rendimento estimado para 1 mês é de " + rendi_stake);
                 break;
 
             case 2:
-                
-                float investimento2 = quant_stake * 0.03f;
 
-                System.out.println("Parabéns! seu lucro foi de "+ investimento2);
-
+                rendi_stake = quant_stake * 0.003f;
+                System.out.println("Rendimento estimado para 3 meses é de " + rendi_stake);
                 break;
 
             case 3:
 
-                float investimento3 = quant_stake * 0.06f;
-
-                System.out.println("Parabéns! seu lucro foi de "+ investimento3);
-
+                rendi_stake = quant_stake * 0.006f;
+                System.out.println("Rendimento estimado para 6 meses é de " + rendi_stake);
                 break;
 
             case 4:
 
-                float investimento4 = quant_stake * 0.09f;
-
-                System.out.println("Parabéns! seu lucro foi de "+ investimento4);
-
+                rendi_stake = quant_stake * 0.009f;
+                System.out.println("Rendimento estimado para 1 ano é de " + rendi_stake);
                 break;
 
             default:
 
-                System.out.println("Opção invalida");
-        
+                System.out.println("Opção inválida!");
+                break;
+
+        }
 
     }
-
- }
 
 }
